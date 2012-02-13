ab97e466fab7d259579b4ca4b9a2fd983fbe011c
mathachew <mmiller@SHLPTPMM.(none)>
2012-02-12 23:47:05 -0600

    Lots-o-improvements
    Temporarily removed auto height adjustment for textarea
    Added link to images on pasted image urls
    Added notifications when a user joins/exits
    Added support for hiding pasted text
    Added cookies to remember nicknames during browser session
    Updated chat window to resize vertically with browser
    Updated SEND button styles
    Added support for getting user list when joining the chat
    Fixed bug that allowed everyone to have the same nickname
    Added support for grouping messages during the same minute

--- server.js
+++ server.js
@@ -9,17 +9,23 @@ app.configure(function() {
 
 app.listen(process.env.C9_PORT || 14243);
 
-var users = {};
+var users = [];
 
 io.sockets.on('connection', function (socket) {
     socket.on('disconnect', function () {
-        var data = { id: socket.id };
-        socket.broadcast.emit('disconnect', data);
-    });
+        var data = {
+            id: socket.id
+        };
 
-    socket.on('mouse-location', function (data) {
-        data.id = socket.id;
-        socket.broadcast.emit('mouse-location', data);
+        for (var i in users) {
+            if (users[i].id == data.id) {
+                data.nickname = users[i].nickname;
+                users.splice(i, 1);
+                break;
+            }
+        }
+
+        socket.broadcast.emit('disconnect', data);
     });
 
     socket.on('message', function (data) {
@@ -27,19 +33,53 @@ io.sockets.on('connection', function (socket) {
         socket.broadcast.emit('message', data);
     });
 
+    socket.on('get-users', function () {
+        socket.emit('get-users', users);
+    });
+
     socket.on('register', function (data) {
         data.id = socket.id;
+
         socket.emit('register', data);
 
         if (data.nickname == 'anon') {
             data.nickname += '-' + data.id;
         }
 
+        users.push({
+            id: socket.id,
+            nickname: data.nickname
+        });
+
         socket.broadcast.emit('new-user', data);
     });
 
     socket.on('update-nickname', function (data) {
         data.id = socket.id;
+
+        for (var i in users) {
+            if (users[i].id == data.id) {
+                users[i].nickname = data.nickname;
+                break;
+            }
+        }
+
         io.sockets.emit('update-nickname', data);
+    });
+
+    socket.on('check-nickname', function (data) {
+        data.id = socket.id;
+        data.exists = false;
+
+        for (var i in users) {
+            if (users[i].nickname.toLowerCase() == data.nickname.toLowerCase()) {
+                if (users[i].id != socket.id) {
+                    data.exists = true;
+                }
+                break;
+            }
+        }
+
+        socket.emit('check-nickname', data);
     });
 });
